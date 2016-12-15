0ae987818a890c0ca6762cf9c2832c80dccea1b6
panos.antonakos <panos.antonakos@upstreamsystems.com>
2016-12-15 19:29:55 +0200

    CHG: Identation

--- src/main/java/com/antonakospanos/Executor.java
+++ src/main/java/com/antonakospanos/Executor.java
@@ -23,32 +23,34 @@ public class Executor {
 		List<String> blackList = methodsToExclude != null ? methodsToExclude : new ArrayList<>();
 		try {
 			classz = (Class<?>) Class.forName(className);
+			
 			Stream.of(classz.getMethods())
 			.filter(m -> m.getParameterCount() == numberOfArgs)
 			.filter(m -> !blackList.contains(m.getName()))
 			.forEach(m -> {
-				// Nested stream to list all method's parameter names
-				System.out.println("The Method '" 
-						+ m.getName() + "' requires the following " 
-						+ m.getParameterCount() + " parameters: " + 
-						Stream.of(m.getParameters())
-							.map(Parameter::getName)
-		          // :: is called 'Method Reference' operator
-		          // creates a function that can be applied.
-							// E.g:
-		          // Function<Parameter, String> f = Parameter::getName;
-							// String name = f.apply();
-							//
-		          // Instead we could use:
-							//.map(p -> p.getName()) 
-							.collect(Collectors.toList())
-							.toString() + "\n"
-						);
-				try {
-					m.invoke(classz.newInstance());
-				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | InstantiationException e) {
-					e.printStackTrace();
-				}
+					// Nested stream to list all method's parameter names
+					System.out.println("The Method '" 
+							+ m.getName() + "' requires the following " 
+							+ m.getParameterCount() + " parameters: " + 
+							Stream.of(m.getParameters())
+								.map(Parameter::getName)
+			          // :: is called 'Method Reference' operator
+			          // creates a Function<Object, Object> that can be applied
+								// and triggers the apply()
+								// E.g:
+			          // Function<Parameter, String> f = Parameter::getName;
+								// String name = f.apply();
+								//
+			          // Instead we could use:
+								//.map(p -> p.getName()) 
+								.collect(Collectors.toList())
+								.toString() + "\n"
+							);
+					try {
+						m.invoke(classz.newInstance());
+					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | InstantiationException e) {
+						e.printStackTrace();
+					}
 			});
 		} catch (ClassNotFoundException e1) {
 			e1.printStackTrace();
